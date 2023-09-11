c6945c77d365ad6d40303073e7142487994ee3a8
DESKTOP-E66TG6M\Nikolic <djordjenikolic001@gmail.com>
2023-09-11 13:31:31 +0200

    simple web input, first version. Preparing for node.js

---
+++ login/main.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('login-button');
+const form = document.getElementById('form-id');
+const aRef = document.getElementById('dont-have');
+
+aRef.addEventListener('click', ()  => {
+    window.location.href = 'http://127.0.0.1:5500/register/register.html';
+})
